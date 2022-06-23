7aa671ce6dcac2f66abb75793cc9f40591689d4f
calebtuttle <1calebtuttle@gmail.com>
2022-06-23 10:51:01 -0400

    Remove apollo. Store static type files instead of relying on apollo code generation.

---
+++ src/@types/subgraph/OpcQuery.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: OpcQuery
+// ====================================================
+
+export interface OpcQuery_opc {
+  __typename: 'OPC'
+  /**
+   * fee in percent for swaps involving OPC approved tokens
+   */
+  swapOceanFee: any | null
+  /**
+   * fee in percent for swaps involving non OPC approved tokens
+   */
+  swapNonOceanFee: any | null
+  approvedTokens: string[] | null
+  id: string
+}
+
+export interface OpcQuery {
+  opc: OpcQuery_opc | null
+}
