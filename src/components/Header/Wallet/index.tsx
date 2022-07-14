b6e929e3371adc694c626de8621c4c058f243dc1
calebtuttle <1calebtuttle@gmail.com>
2022-07-14 13:08:53 -0400

    Remove more Ocean code

--- src/components/Header/Wallet/index.tsx
+++ src/components/Header/Wallet/index.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react'
 import Account from './Account'
 import Details from './Details'
 import Tooltip from '@shared/atoms/Tooltip'
-import Network from './Network'
 import styles from './index.module.css'
 import { useWeb3 } from '@context/Web3'
 
@@ -11,7 +10,6 @@ export default function Wallet(): ReactElement {
 
   return (
     <div className={styles.wallet}>
-      <Network />
       <Tooltip
         content={<Details />}
         trigger="click focus"
